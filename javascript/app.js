bd29905f2469b8615713b32ef4c7d83762b9f505
Paola Cerioli <pcerioli@gmail.com>
2013-09-07 16:12:21 -0700

    second commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,21 +1,12 @@
-/*global angular */
-/*jshint unused:false */
-//'use strict';
-
-/**
- * The main TodoMVC app module
- *
- * @type {angular.Module}
- */
 var wordApp = angular.module('wordApp', ['firebase']);
 
 /*  ROUTING */
 wordApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
   $routeProvider.
-  	when('/', { templateUrl: 'pages/home.html', controller: 'GameCtrl' }).
-  	when('/login', { templateUrl: 'pages/login.html', controller: 'GameCtrl'  }).
-  	when('/game', { templateUrl: 'pages/game.html', controller: 'GameCtrl'  }).
+  	when('/', { templateUrl: 'pages/home.html' }).
+  	when('/login', { templateUrl: 'pages/login.html' }).
+  	when('/game', { templateUrl: 'pages/game.html' }).
   	when('/privacy', { templateUrl: 'pages/privacy.html' }).
   	when('/terms', { templateUrl: 'pages/terms.html' }).
   	otherwise({ redirectTo: '/' });
@@ -24,34 +15,8 @@ wordApp.config(['$routeProvider', '$locationProvider', function ($routeProvider,
 
 }]);
 
-/*  AUTHENTICATION */
-// wordApp.run(['$rootScope', 'authManager', function($rootScope, authManager) {
-//  $rootScope.login = authManager.login;
-//  $rootScope.logout = authManager.logout;
-// }]);
 
 
 
 
 
-
-// wordApp.filter('wordFilter', function ($location) {
-// 	return function (input) {
-// 		var filtered = {};
-// 		angular.forEach(input, function (todo, id) {
-// 			var path = $location.path();
-// 			if (path === '/active') {
-// 				if (!todo.completed) {
-// 					filtered[id] = todo;
-// 				}
-// 			} else if (path === '/completed') {
-// 				if (todo.completed) {
-// 					filtered[id] = todo;
-// 				}
-// 			} else {
-// 				filtered[id] = todo;
-// 			}
-// 		});
-// 		return filtered;
-// 	};
-// });
--- javascript/gameCtrl.js
+++ javascript/gameCtrl.js
@@ -1,39 +1,51 @@
-'use strict';
-
-
 wordApp.controller('GameCtrl', function GameCtrl($scope, $location, Game, angularFire) {
 
   //var url = 'https://word-gaming.firebaseio.com/';
   var dbRef = new Firebase('https://word-gaming.firebaseio.com/');
-  var wordsRef = dbRef.child('words');
-  var gamesRef = dbRef.child('games');
+  //var dbRef = new Firebase('https://wordbang.firebaseio.com/');
 
-  angularFire(wordsRef.limit(10), $scope, "words");
+  //var wordsRef = dbRef.child('words');
+  var gamesRef = dbRef.child('games');
+  var opengamesRef = dbRef.child('opengames');
+
+  //angularFire(wordsRef.limit(10), $scope, "words");
   angularFire(gamesRef.limit(10), $scope, "games");
+  angularFire(opengamesRef.limit(20), $scope, "opengames");
 
   //var url = 'https://todomvc-angular.firebaseio.com/';
-  $scope.newWord = '';
-  $scope.user = '';
-  $scope.existingWords = ['test'];
+  //$scope.newWord = '';
+  //$scope.username = 'paola';
+  $scope.user = {};
 
-  $scope.addGame = function() {
+  //$scope.existingWords = ['test'];
 
-    $scope.games[gamesRef.push().name()] = {
-      player: $scope.username
+//  $scope.addGame = function() {
+//
+//    $scope.games[gamesRef.push().name()] = {
+//      player: $scope.username
+//    };
+//    $location.path( "/game" );
+//  };
+
+  $scope.addOpengame = function() {
+    console.log("username = " + $scope.user.name);
+
+    $scope.opengames[opengamesRef.push().name()] = {
+        player: $scope.user.name
     };
-    $location.path( "/game" );
   };
 
-  $scope.addWord = function() {
-    //console.log($scope.words);
-    //console.log("newWord is " + $scope.newWord);
-    
-    var points = $scope.newWord.length;
-    $scope.words[wordsRef.push().name()] = {
-      username: $scope.user.name, content: $scope.newWord, points: points
-    };
-    $scope.newWord = "";
-  };
+
+//  $scope.addWord = function() {
+//    //console.log($scope.words);
+//    //console.log("newWord is " + $scope.newWord);
+//
+//    var points = $scope.newWord.length;
+//    $scope.words[wordsRef.push().name()] = {
+//      username: $scope.user.name, content: $scope.newWord, points: points
+//    };
+//    $scope.newWord = "";
+//  };
 
  
 });
