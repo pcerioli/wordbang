6debec029a69e786a02c57b19dc95813ab8ecfd8
Paola Cerioli <pcerioli@gmail.com>
2013-09-08 13:24:47 -0700

    last commit

--- javascript/main.js
+++ javascript/main.js
@@ -13,7 +13,7 @@ wordApp.controller('SplashCtrl', ['$scope', '$timeout', 'angularFireCollection',
     $scope.deleteAll = function() {
       var allWords = new Firebase('https://word-gaming.firebaseIO-demo.com/words/');
       allWords.remove();
-    }
+    };
 
     $scope.addWord = function() {
       if ($scope.newWord != null) {
@@ -42,11 +42,57 @@ wordApp.controller('SplashCtrl', ['$scope', '$timeout', 'angularFireCollection',
         $('#invalidAlert').show().fadeOut(2000);
         //$('#invalidAlert').hide();
       }
-    }
+    };
 
+
+    var ref = new Firebase('https://word-gaming.firebaseio.com');
+
+    var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+      if (error) {
+        // an error occurred while attempting login
+        alert(error);
+      } else if (user) {
+
+        var get_url='https://graph.facebook.com/'+ user.id +'/?fields=picture&access_token='+user.accessToken;
+
+        console.log("get_url: " + get_url);
+
+        $.getJSON(get_url, function(data) {
+          var items = [];
+          _userPhoto = data.picture.data.url;
+
+          console.log("photo_url;: " + data.picture.data.url);
+
+
+          //document.getElementById("image").innerHTML="<img src='" +  data.picture.data.url+"'>";
+          //document.getElementById("userName").innerHTML=user.displayName;
+
+          //var newUserRef = new Firebase('https://word-gaming.firebaseio.com/users/');
+          //passing useri
+          //newUserRef.set(user.id);
+          $scope.username = user.displayName;
+          $scope.userid = user.id;
+          $scope.userPicture = _userPhoto;
+
+          //:{username : user.displayName , photo : data.picture.data.url} });
+
+        });
+
+      }
+    });
+
+    // attempt to log the user in with your preferred authentication provider
+    auth.login('facebook', {
+      rememberMe: true,
+      scope: 'email,user_likes,user_photos'
+    });
 
   }
-])
+
+
+
+]);
+
 wordApp.directive('autoScroll', function($timeout) {
   return function(scope, elements, attrs) {
     scope.$watch("words.length", function() {
